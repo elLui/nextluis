c2e0f0b058abf939a2b3d438d0669c4e6bd48ef2
elluis <elluis.codes@gmail.com>
2023-02-08 21:17:41 -0600

    alright, got google fonts up and running - brought in 4 promising looking fonts that look pretty versatile -
    1. strip out the old stuff

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,17 @@
 import 'sanitize.css';
 import 'sanitize.css/forms.css';
 import 'sanitize.css/typography.css';
-import { Audiowide, Comfortaa, Montserrat, Quicksand, Staatliches } from "@next/font/google";
+import { comfortaa } from "@/app/styles/fonts/fonts";
 import styled, { ThemeProvider } from "styled-components";
 import StyledComponentsRegistry from "./lib/StyledComponentsRegistry.jsx";
-
-
-const audiowide = localFont({
-    src: "../pu"
-                            })
-
+import { Comfortaa } from "./styles/fonts/fonts";
 
 
 export default function RootLayout( { children } ) {
-    return ( <html lang="en">
+    return ( <html
+        lang="en"
+        className={comfortaa.className}
+    >
     {/*
      <head /> will contain the components returned by the nearest parent
      head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
---
+++ app/styles/fonts/fonts.jsx
@@ -0,0 +1,16 @@
+import { Comfortaa, Fredoka, MuseoModerno, Raleway } from "@next/font/google";
+
+
+
+export const comfortaa = Comfortaa( {
+                                        subsets : [ "latin" ], display : "swap",
+                                    } );
+export const fredoka = Fredoka( {
+                                    subsets : [ "latin" ], display : "swap",
+                                } );
+export const museoModerno = MuseoModerno( {
+                                              subsets : [ "latin" ], display : "swap",
+                                          } );
+export const raleway = Raleway( {
+                                    subsets : [ "latin" ], display : "swap",
+                                } );
